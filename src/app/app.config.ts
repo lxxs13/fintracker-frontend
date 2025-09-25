b64f47ed16f2d3223596536a3d49cd36f7700374
lxxs13 <luis.quiroz90@gmail.com>
2025-09-25 12:24:42 -0600

    Adding preset primeNG

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,24 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
+import { providePrimeNG } from 'primeng/config';
+
 import { routes } from './app.routes';
+import { MyPreset } from '../mypreset';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+    providePrimeNG({
+      theme: {
+        preset: MyPreset,
+        options: {
+          darkModeSelector: true,
+        }
+      }
+    }),
   ]
 };
+
