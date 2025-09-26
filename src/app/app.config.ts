c877851cb4b1807fa2e6edff990b438932c86744
lxxs13 <luis.quiroz90@gmail.com>
2025-09-26 08:42:42 -0600

    Adding login service with connection to backend

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,11 +7,14 @@ import { providePrimeNG } from 'primeng/config';
 
 import { routes } from './app.routes';
 import { MyPreset } from '../mypreset';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { GlobalInterceptor } from './core/interceptors/global-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
+    provideHttpClient(withInterceptors([GlobalInterceptor])),
     provideRouter(routes),
     provideNoopAnimations(),
     providePrimeNG({
--- src/app/core/auth/services/auth.ts
+++ src/app/core/auth/services/auth.ts
@@ -1,9 +1,16 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ILoginDTO } from '../../../models/DTOS/ILoginDTO';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private _http = inject(HttpClient);
 
+  login(loginData: ILoginDTO): Observable<any> {
+    return this._http.post('/auth', loginData);
+  }
 
 }
