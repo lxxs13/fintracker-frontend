3c73593406d2858080b7997cdf39bf01687cae4c
lxxs13 <luis.quiroz90@gmail.com>
2025-09-29 14:31:09 -0600

    Creating categories settings page

---
+++ src/app/features/settings/components/categories/categories.ts
@@ -0,0 +1,51 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { CardModule } from 'primeng/card';
+import { DividerModule } from 'primeng/divider';
+import { ScrollPanelModule } from 'primeng/scrollpanel';
+import { TabsModule } from 'primeng/tabs'
+import { CategoriesService } from '../../services/categories';
+import { ICategories, ICategoriesListResponse } from '../../../../models/responses/ICategoriesListResponse';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { SelectButtonModule } from 'primeng/selectbutton';
+import { IconColorClassPipe } from '../../../../shared/pipes/icon-color-class-pipe';
+
+
+@Component({
+  selector: 'fintracker-categories',
+  imports: [
+    FormsModule,
+    CommonModule,
+    CardModule,
+    ScrollPanelModule,
+    TabsModule,
+    DividerModule,
+    SelectButtonModule,
+    IconColorClassPipe,
+  ],
+  templateUrl: './categories.html',
+})
+export class CategoriesComponent implements OnInit {
+  private _categoryService = inject(CategoriesService);
+  selectedIndexTab: number = 0;
+
+  spentList: ICategories[] = [];
+  selectedSpend: ICategories | undefined;
+
+  incomesList: ICategories[] = [];
+  selectedIncome: ICategories | undefined;
+
+
+  ngOnInit(): void {
+    this._categoryService.GetCategoriesByUserId().subscribe({
+      next: ((response: ICategoriesListResponse) => {
+        const { categoriesSpent, categoriesIncome } = response;
+
+        this.spentList = categoriesSpent;
+        this.incomesList = categoriesIncome;
+      }),
+      error: (err) => console.error(err)
+    })
+  }
+
+}
---
+++ src/app/features/settings/services/categories.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ICategoriesListResponse } from '../../../models/responses/ICategoriesListResponse';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoriesService {
+  private _http = inject(HttpClient);
+
+  GetCategoriesByUserId(): Observable<ICategoriesListResponse> {
+    return this._http.get<ICategoriesListResponse>('/categories');
+  }
+
+}
