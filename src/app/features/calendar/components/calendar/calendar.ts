ead019805c9bba9a0ad1e6b222798b8af1823fd7
lxxs13 <luis.quiroz90@gmail.com>
2025-10-17 15:19:22 -0600

    Updating calendar to show transactions by day

--- src/app/features/calendar/components/calendar/calendar.ts
+++ src/app/features/calendar/components/calendar/calendar.ts
@@ -5,10 +5,14 @@ import { CardModule } from 'primeng/card';
 import { DatePickerModule } from 'primeng/datepicker';
 import { AccordionModule } from 'primeng/accordion';
 import { TransactionService } from '../../../transactions/services/transaction';
-import { ITransactionList } from '../../../../models/responses/ITransactionsListResponse';
+import { ITransactionItem } from '../../../../models/responses/ITransactionsListResponse';
 import { IconColorClassPipe } from '../../../../shared/pipes/icon-color-class-pipe';
 import { CommonService } from '../../../../shared/services/common';
 import { ITransactionsFilterDTO } from '../../../../models/ITransactionsFilterDTO';
+import { Transaction } from '../../../transactions/components/transaction/transaction';
+import { AccountService } from '../../../accounts/services/account';
+import { EAccountType, EAccountTypeMapperText } from '../../../../enums/AccountTypes';
+import { IAccount, IAccountsListResponse } from '../../../../models/responses/IDebitAccountsResponse';
 
 interface Tx {
   transactionDate: string | Date; // ISO o Date
@@ -34,13 +38,24 @@ interface Tx {
 })
 export class CalendarComponent implements OnInit {
   private _transactionService = inject(TransactionService);
-  private _commonService = inject(CommonService)
+  private _accountService = inject(AccountService);
 
-  transactionList: ITransactionList[] = [];
-  originalTransactionList: ITransactionList[] = [];
+  commonService = inject(CommonService)
+
+  transactionList: ITransactionItem[] = [];
+  originalTransactionList: ITransactionItem[] = [];
 
   date: Date[] = [];
   lastDayOfMonth: Date | null = null;
+
+  transactionsByDay: Map<string, ITransactionItem[]> = new Map();
+  creditCardsBalance: number = 0;
+  remappedCreditList: IAccount[] = [];
+  accountsBalanceTotal: number = 0;
+  remappedAccountsList: any;
+
+  debitAccounts: IAccount[] = [];
+  creditAccounts: IAccount[] = [];
 
   ngOnInit(): void {
     this.date?.push(new Date());
@@ -52,13 +67,50 @@ export class CalendarComponent implements OnInit {
   updateLastDay(event: any) {
     this.lastDayOfMonth = new Date(event.year, event.month, 0);
     this.getTransactions();
+    // this.getAccounts();
+  }
+
+  getAccounts() {
+    this._accountService.GetDebitCardsByUser().subscribe({
+      next: (response: IAccountsListResponse) => {
+        const { debitAccounts, creditAccounts } = response;
+
+        this.debitAccounts = debitAccounts;
+        this.creditAccounts = creditAccounts;
+
+        const balanceCreditCards = creditAccounts.reduce((sum, creditAccounts) => sum + creditAccounts.currentBalance, 0);
+        this.creditCardsBalance = balanceCreditCards;
+
+        this.remappedCreditList = creditAccounts;
+
+        const result = Object.groupBy(debitAccounts, ({ accountType }) => accountType);
+
+        for (const key in result) {
+          const typedKey = Number(key) as EAccountType;
+          const mappedKey = EAccountTypeMapperText[typedKey];
+          const accounts = result[key];
+
+          if (mappedKey && accounts) {
+            const totalBalance = accounts.reduce((sum, accounts) => sum + accounts.currentBalance, 0);
+
+            this.accountsBalanceTotal += totalBalance;
+
+            this.remappedAccountsList.push({
+              type: mappedKey,
+              accounts,
+              totalBalance,
+            });
+          }
+        }
+      }
+    });
   }
 
   getTransactions(): void {
     const start = new Date(this.lastDayOfMonth!.getFullYear(), this.lastDayOfMonth!.getMonth(), 1, 0, 0, 0, 0);
     const end = new Date(this.lastDayOfMonth!.getFullYear(), this.lastDayOfMonth!.getMonth() + 1, 0, 23, 59, 59, 999);
 
-    const { startDate, endDate } = this._commonService.getDates([start, end]);
+    const { startDate, endDate } = this.commonService.getDates([start, end]);
 
     const filters: ITransactionsFilterDTO = { startDate, endDate };
 
@@ -69,20 +121,53 @@ export class CalendarComponent implements OnInit {
         // this.totalSpent = spentTotal;
         this.transactionList = transactionList;
         this.originalTransactionList = this.transactionList;
+
+        this.groupTransactionsByDay();
       }
     })
   }
 
+  groupTransactionsByDay() {
+    this.transactionsByDay.clear();
+
+    this.transactionList.forEach(transaction => {
+      // Crear key en formato 'YYYY-MM-DD'
+      const dateKey = new Date(transaction.transactionDate).toISOString().split('T')[0];
+
+      if (!this.transactionsByDay.has(dateKey)) {
+        this.transactionsByDay.set(dateKey, []);
+      }
+
+      this.transactionsByDay.get(dateKey)!.push(transaction);
+    });
+  }
+
+  totalForCell(item: any): number {
+    const year = item.year;
+    const month = String(item.month + 1).padStart(2, '0');
+    const day = String(item.day).padStart(2, '0');
+
+    const dateKey = `${year}-${month}-${day}`;
+    const dayTransactions = this.transactionsByDay.get(dateKey) || [];
+
+    return dayTransactions.reduce((sum, transaction) => sum + this.commonService.getAbsAmount(transaction.amount), 0);
+  }
+
   filterListBySelectedDay(event: Date) {
-    const { startDate: selectedStartDay, endDate: selectedEndDay } = this._commonService.getDates([event]);
+    const { startDate: selectedStartDay, endDate: selectedEndDay } = this.commonService.getDates([event]);
+
     this.transactionList = this.originalTransactionList.filter(element => {
-      const transactionDate = new Date(element.transactionDate).getTime();
+      const transactionDate = new Date(element.transactionDate);
+      transactionDate.setHours(0, 0, 0, 0);
 
-      const initalDate = this.parseDDMMYYYY(selectedStartDay).getTime();;
-      const endDate = this.parseDDMMYYYY(selectedEndDay).getTime();
+      const initialDate = this.parseDDMMYYYY(selectedStartDay);
+      initialDate.setHours(0, 0, 0, 0);
 
+      const endDate = this.parseDDMMYYYY(selectedEndDay);
+      endDate.setHours(23, 59, 59, 999);
 
-      return transactionDate >= initalDate && transactionDate <= endDate;
+      return transactionDate.getTime() >= initialDate.getTime() &&
+        transactionDate.getTime() <= endDate.getTime();
     });
   }
 
@@ -100,8 +185,5 @@ export class CalendarComponent implements OnInit {
     this.ngOnInit();
   }
 
-  totalForCell(date: Date): number {
 
-    return 543345;
-  }
 }
